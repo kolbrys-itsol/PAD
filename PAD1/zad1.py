32229113521a862c8bb63c7badba7f16bec2cc2b
s18310 <s18310@pjwstk.edu.pl>
2022-03-25 12:32:19 +0100

    initial commit for PAD1

---
+++ PAD1/zad1.py
@@ -0,0 +1,88 @@
+# zadanie 1
+class Animal:
+
+    def __init__(self, gender='Female', genus=None):
+        self.isAlive = True
+        self.gender = gender
+        self.genus = genus
+
+    def breed(self, partner):
+        if self.gender == 'Female' and partner.gender == 'Male' and self.genus == partner.genus:
+            if self.genus == 'Canis':
+                return Dog()
+            else:
+                return Cat()
+        else:
+            raise Exception('attribute not found')
+
+    @property
+    def genus(self):
+        return self._genus
+
+    @genus.setter
+    def genus(self, new_genus):
+        if new_genus is None or new_genus == 'Canis' or new_genus == 'Felis':
+            self._genus = new_genus
+        else:
+            raise Exception('Wrong genus type!')
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, new_gender):
+        if new_gender == 'Female' or new_gender == 'Male':
+            self._gender = new_gender
+        else:
+            raise Exception('Wrong gender type!')
+
+
+class Dog(Animal):
+    def __init__(self, gender='Female', genus='Canis'):
+        Animal.__init__(self, gender=gender, genus=genus)
+
+    def woof(self):
+        return "woof woof"
+
+    @Animal.genus.setter
+    def genus(self, new_genus):
+        if new_genus == "Canis":
+            self._genus = new_genus
+        else:
+            raise Exception('Dog can only be Canis')
+
+
+class Cat(Animal):
+    def __init__(self, gender='Female', genus='Felis'):
+        Animal.__init__(self, gender=gender, genus=genus)
+
+    def purr(self):
+        return "purr"
+
+    @Animal.genus.setter
+    def genus(self, new_genus):
+        if new_genus == "Felis":
+            self._genus = new_genus
+        else:
+            raise Exception('Cat can only be Feline')
+
+
+if __name__ == '__main__':
+    test = Animal('Male', 'Canis')
+    print(test.genus)
+    print('======')
+    test2 = Dog('Male')
+    print(test2.woof())
+    print('======')
+    cat = Cat('Male')
+    print(cat.purr())
+    print('======')
+    cat2 = Cat('Female')
+    cat3 = cat2.breed(cat)
+    print(cat3.genus)
+    print(cat3.purr())
+
+# zadanie 2
+
+
---
+++ PAD1/zad2.py
@@ -0,0 +1,55 @@
+class Worker:
+    def __init__(self, number, name, age, salary):
+        self.number = number
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+
+def average_salary(workers):
+    num_of_workers = len(workers)
+    salary_sum = 0
+    for w in workers:
+        salary_sum += w.salary
+    return salary_sum / num_of_workers
+
+
+def average_salary_age(workers):
+    try:
+        below_30 = 0
+        below_30_sum = 0
+        older = 0
+        older_sum = 0
+
+        for w in workers:
+            if 2022 - w.age < 30:
+                below_30 += 1
+                below_30_sum += w.salary
+            else:
+                older += 1
+                older_sum += w.salary
+        result = (below_30_sum / below_30) - (older_sum / older)
+        if result < 0:
+            print('Średnie zarobki osób poniżej 30 roku życia są mniejsze o', "{:.2f}".format(abs(result)))
+        elif result > 0:
+            print('Średnie zarobki osób poniżej 30 roku życia są większe o', "{:.2f}".format(result))
+        else:
+            print('Zarobki są równe')
+    except ZeroDivisionError:
+        print('Jedna z grup wiekowych jest pusta!')
+
+
+if __name__ == '__main__':
+    w1 = Worker(1, 'Adam', 1983, 1500)
+    w2 = Worker(2, 'Anna', 1981, 1700)
+    w3 = Worker(3, 'Błażej', 1990, 1800)
+    w4 = Worker(4, 'Beata', 1993, 1600)
+    w5 = Worker(5, 'Czesław', 1980, 2000)
+    w6 = Worker(6, 'Cecylia', 1983, 2100)
+    w7 = Worker(7, 'Daniel', 1976, 1900)
+
+    workers = [w1, w2, w3, w4, w5, w6, w7]
+    print('Średnia pensja:')
+    print(average_salary(workers))
+    print('Średnia wg wieku:')
+    average_salary_age(workers)
