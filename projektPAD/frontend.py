2dd86b4a30674fe96501069338cc33f58538277d
s18310 <s18310@pjwstk.edu.pl>
2022-06-25 22:03:32 +0200

    completed work on PAD project

--- projektPAD/frontend.py
+++ projektPAD/frontend.py
@@ -12,12 +12,13 @@ if content == 'Strona główna':
 else:
     file = st.file_uploader('Wprowadź plik data.csv utworzony przez web scraper', type=['csv'])
     if file is not None:
-        with st.spinner('Wczytuję plik...'):
+        with st.spinner('Poczekaj...'):
             time.sleep(1)
         dataframe = pd.read_csv(file, sep=',')
 
         graph = st.selectbox('Wybierz rodzaj wykresu',
-                             ['Wybierz', 'Szczegółowość tagów a popularność', 'Treści dla dorosłych', 'Scatter'])
+                             ['Wybierz', 'Szczegółowość tagów a popularność', 'Treści dla dorosłych', 'VN przez lata',
+                              'Popularność a długość', 'Ocena a długość'])
 
         columns = dataframe.columns.to_list()
         if graph == 'Szczegółowość tagów a popularność':
@@ -27,9 +28,9 @@ else:
                 'Maksymalna liczba ogólnych tagów to 30.')
             dataframe['Count'] = dataframe.Tags.transform(ast.literal_eval).str.len()
             fig = plot.line(dataframe, facet_row_spacing=0.05, x='Popularity', y='Count')
-            # fig.update_yaxes(autorange="reversed")
             st.plotly_chart(fig)
-            st.markdown('Widzimy, że im mniejsza popularność, tym większa szansa, że pozycja ma mniej szczegółowy opis.')
+            st.markdown(
+                'Widzimy, że im mniejsza popularność, tym większa szansa, że pozycja ma mniej szczegółowy opis.')
         elif graph == 'Treści dla dorosłych':
             mask_nsc = dataframe.Tags.apply(lambda x: 'No Sexual Content' in x)
             df2 = dataframe[mask_nsc]
@@ -43,3 +44,38 @@ else:
                 'Większość gier typu visual novel (zwłaszcza japońskich) zawiera treści dla dorosłych. '
                 'Sprawdźmy jak to wygląda w przypadku 1500 najpopularniejszych gier na VNDB:')
             st.plotly_chart(fig)
+        elif graph == 'VN przez lata':
+            df2 = dataframe
+            df2['Date'] = pd.to_datetime(df2['Date'], errors='coerce')
+            fig = plot.line(df2.Id.groupby(by=[df2.Date.dt.year]).agg('count'), labels={
+                'value': 'Quantity',
+                'variable': 'Count',
+                'Date': 'Year'
+            })
+            st.header('VN przez lata')
+            st.subheader(
+                'Widzimy duży wzrost ilości wydanych visual novels po roku 2004 '
+                '(premiera Fate/Stay Night). Według bazy najwięcej tytułów powstało w 2016 roku. '
+                'Miejmy na uwadze że sporo najnowszych pozycji mogło nie odnaleźć się jeszcze w VNDB. '
+                'Zwłaszcza pozycje, które nie zostały przetłumaczone na język angielski.')
+            st.plotly_chart(fig)
+        elif graph == 'Popularność a długość':
+            df2 = dataframe
+            fig = plot.pie(df2.Length.groupby(by=[df2.Length]).agg('count').reset_index(name='Count'), names='Length',
+                           values='Count')
+            st.header('Popularność a długość')
+            st.subheader('Rozkład visual novels według ich zadeklarowanej długości.')
+            st.plotly_chart(fig)
+
+        elif graph == 'Ocena a długość':
+            df2 = dataframe
+            print(df2.Rating.groupby(by=[df2.Length]).agg('mean').reset_index(name='Mean'))
+            # fig = plot.bar(df2.Rating.groupby(by=[df2.Length]).agg('mean').reset_index(name='Mean'), x='Length',
+            #                y='Mean')
+            fig = plot.bar(df2.Rating.groupby(by=[df2.Length]).agg('mean').reset_index(name='Mean'), x='Length',
+                            y='Mean')
+            fig.update_xaxes(categoryorder='array',
+                             categoryarray=['Not available', 'Very short', 'Short', 'Medium', 'Long', 'Very long'])
+            st.header('Ocena a długość')
+            st.plotly_chart(fig)
+            st.markdown('Widzimy że średnia ocena rośnie wraz z długością gry!')
